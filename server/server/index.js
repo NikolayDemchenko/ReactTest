6c50d65ac2daeafcada07b61b6b35975f1153b7f
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2019-11-24 20:00:49 +0300

    Настройка 02

---
+++ server/server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer } = require("apollo-server-express");
+const mongoose = require("mongoose");
+
+const startServer = async () => {
+  const app = express();
+  app.use(cors());
+
+  const server = new ApolloServer({
+    modules: [
+      require("../models/Folder"),
+      require("../models/Template")
+    ]
+  });
+  server.applyMiddleware({ app, path: "/graphql" });
+ 
+  const localURI = "mongodb://localhost:27017/LocalMongoBase";
+
+  await mongoose.connect(localURI, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function (err) {
+    if (err) throw err;
+    console.log('Connected to DB!');
+  });
+
+  const port = 8000;
+  app.listen({ port }, () => {
+    console.log(`Apollo Server on http://localhost:${port}/graphql`);
+  });
+};
+startServer();
