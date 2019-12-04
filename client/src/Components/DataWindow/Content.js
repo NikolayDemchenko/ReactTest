766f2583b300c53353b5d03d12d73330d50b0770
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2019-12-04 19:42:59 +0300

    client Folder 01

--- client/src/Components/DataWindow/Content.js
+++ client/src/Components/DataWindow/Content.js
@@ -1,27 +1,43 @@
 import React from "react";
 import style from "./Content.module.css";
-import { useQuery } from '@apollo/react-hooks';
-import {GET_FOLDER_CHILDS} from './folderQueries'
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { GET_FOLDER_CHILDS, ADD_FOLDER,UPDATE_FOLDER } from './folderQueries'
 
 
 export default function Content() {
   let parentId = null;
+  const [updateFolder] = useMutation(UPDATE_FOLDER);
   const { loading, error, data } = useQuery(GET_FOLDER_CHILDS, {
-    variables: {parentId}
+    variables: { parentId }
   });
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
-const handleClick=(item)=>{
-  parentId=item;
-  console.log(parentId);
-}
+  const handleClick = (item) => {
+    parentId = item;
+    console.log(parentId);
+  }
+
+  let input;
   const items = data.childFolders.map(folder => (
-    <div onClick={() => handleClick(folder)} className={style.Item} key={folder.id}>{folder.name}</div>
+    <div onClick={() => handleClick(folder)} className={style.Item} key={folder.id}>
+      <input ref={node => {
+        input = node;
+      }} className={style.Input} defaultValue=
+        {folder.name} />
+        <button onClick={e => {
+          e.preventDefault();
+          updateFolder({ variables: {id:folder.id, name: input.value, parentId: null } });
+          input.value = "";
+        }}>Применить</button>      
+    </div>
   ))
   console.log(data.childFolders);
 
+  
   return (
-    <div className={style.Content}>{items}</div>
+    <div className={style.Content}>{items}
+      <button>Добавить</button>
+    </div>
   );
 };
--- client/src/Components/DataWindow/folderQueries.js
+++ client/src/Components/DataWindow/folderQueries.js
@@ -12,8 +12,6 @@ const GET_ALL_FOLDERS = gql`
   }
 }
 `;
-
-
 const GET_FOLDER_BY_ID = gql`
 query ($id: ID){
   folder(id:$id){
@@ -27,8 +25,46 @@ query ($id: ID){
   }
 }
 `;
-const name ='name'
-const subData =`{id ${name} parentId}`
-const dataBody = `query($parentId:ID){childFolders(parentId:$parentId)${subData}}`
-const GET_FOLDER_CHILDS = gql`${dataBody}`;
-export { GET_ALL_FOLDERS, GET_FOLDER_BY_ID, GET_FOLDER_CHILDS };
+const GET_FOLDER_CHILDS = gql`
+query($parentId:ID){
+  childFolders(parentId:$parentId){
+    id
+    name
+    parentId
+    }
+  }
+`;
+// Создание объекта Folder
+const ADD_FOLDER = gql`
+mutation($name:String,$parentId:ID) {
+  addFolder(name:$name,parentId:$parentId) {
+    id
+    name
+    parentId
+    folders{
+      name
+    }
+  }
+}
+`;
+// Обновление объекта Folder
+const UPDATE_FOLDER = gql`
+mutation($id:ID!,$name:String!,$parentId:ID) {
+  updateFolder(id:$id,name:$name,parentId:$parentId) {
+    id
+    name
+    parentId
+    folders{
+      name
+    }
+  }
+}
+`;
+export { GET_ALL_FOLDERS, GET_FOLDER_BY_ID, GET_FOLDER_CHILDS, ADD_FOLDER, UPDATE_FOLDER };
+
+
+// Составной запрос
+// const name = 'name'
+// const subData = `{id ${name} parentId}`
+// const dataBody = `query($parentId:ID){childFolders(parentId:$parentId)${subData}}`
+// const GET_FOLDER_CHILDS = gql`${dataBody}`;
