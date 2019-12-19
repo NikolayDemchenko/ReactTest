ec17fcffcb618f91c78df516d19d197cd8daa11c
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2019-12-19 20:13:17 +0300

    Folder CascadeDelete updated

--- server/models/Folder/cascadeDelete.js
+++ server/models/Folder/cascadeDelete.js
@@ -1,8 +1,8 @@
 const cascadeDelete = async (id, items) => {
   const childrens = await items.find({ parentId: id });
   if (childrens != []) {
-    childrens.forEach(item => {
-    cascadeDelete(item.id,items);
+    childrens.forEach(({id}) => {
+    cascadeDelete(id,items);
     });
   }
     await items.findByIdAndDelete(id);
