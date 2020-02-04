e4f6eedd7f23c5dd3d843280d9206ee5d424e7d9
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2020-02-04 04:43:49 +0300

    bc modification

---
+++ client/src/Components/Buttons/ButtonsContainer.js
@@ -0,0 +1,11 @@
+import buttons from './AllButtons'
+import ActiveButton from './ActiveButton'
+import React from 'react'
+
+export default function ButtonsContainer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
