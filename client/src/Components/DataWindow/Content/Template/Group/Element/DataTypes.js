d0ce420ee85851a1cfc288903038e5a6f34e7730
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2020-02-16 16:55:27 +0300

    Element proekt_01

---
+++ client/src/Components/DataWindow/Content/Template/Group/Element/DataTypes.js
@@ -0,0 +1,28 @@
+Структура элемента
+
+1 Global settings:
+  1 nameSettings:
+    1 input 
+    2 BtnContainer:
+      1 btn nameVisible: (on/off)
+  2 Settings:
+    2 BtnContainer:
+      1 btn globalVisible: (on/off) 
+      2 btn Type: (data / inpit)
+      3 btn filter:(on/off)
+
+2 Data:
+  1 choice Data types:
+    1 name: "Колекция" type: Array
+      1 Collection View:
+        1 Folder :{Icon,name}
+        2 Row:{Icon,name,settings,data}
+    2 name: "Строка" type:String 
+    3 name: "Текст" type:String 
+    4 name: "Число" type:Number 
+    5 name: "Изображение" type:String 
+      1  btn iconImage:(on/off)  
+    6 name: "Видео" type:String 
+    7 name: "Аудио" type:String 
+
+3 Динамический блок ввода и отображения данных
