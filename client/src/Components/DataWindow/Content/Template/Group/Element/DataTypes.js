5cb8690fba224a62090c311e5b60a1eaaf6a2db0
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2020-02-17 19:48:39 +0300

    Element proekt_01_17022020

--- client/src/Components/DataWindow/Content/Template/Group/Element/DataTypes.js
+++ client/src/Components/DataWindow/Content/Template/Group/Element/DataTypes.js
@@ -1,28 +1,51 @@
-Структура элемента
+// Структура элемента
+export const Name = ({ name }) => {
+  return (
+    <div>
+      <input value={name} />
+      <BtnsContainer exist={} visible={} />
+    </div>
+  );
+};
+export const Element = ({ name, nameVisible }) => {
+  return (
+    <div>
+      <Name name={name} visible={} />
+      <Type />
+      <Settings visible={} type={} />
+    </div>
+  );
+};
 
-1 Global settings:
-  1 nameSettings:
-    1 input 
-    2 BtnContainer:
-      1 btn nameVisible: (on/off)
-  2 Settings:
-    2 BtnContainer:
-      1 btn globalVisible: (on/off) 
-      2 btn Type: (data / inpit)
-      3 btn filter:(on/off)
+const Solution = {
+  KeyValue: { key: value },
+  Container:{ContainerType, value: [KeyValue] },
 
-2 Data:
-  1 choice Data types:
-    1 name: "Колекция" type: Array
-      1 Collection View:
-        1 Folder :{Icon,name}
-        2 Row:{Icon,name,settings,data}
-    2 name: "Строка" type:String 
-    3 name: "Текст" type:String 
-    4 name: "Число" type:Number 
-    5 name: "Изображение" type:String 
-      1  btn iconImage:(on/off)  
-    6 name: "Видео" type:String 
-    7 name: "Аудио" type:String 
+  value: Container || KeyValue || Row || Text || Image || Video || Audio 
+};
+// 1 Global settings:
+//   1 nameSettings:
+//     1 input
+//     2 BtnContainer:
+//       1 btn nameVisible: (on/off)
+//   2 Settings:
+//     2 BtnContainer:
+//       1 btn globalVisible: (on/off)
+//       2 btn Type: (data / inpit)
+//       3 btn filter:(on/off)
 
-3 Динамический блок ввода и отображения данных
+// 2 Data:
+//   1 choice Data types:
+//     1 name: "Колекция" type: Array
+//       1 Collection View:
+//         1 Folder :{Icon,name}
+//         2 Row:{Icon,name,settings,data}
+//     2 name: "Строка" type:String
+//     3 name: "Текст" type:String
+//     4 name: "Число" type:Number
+//     5 name: "Изображение" type:String
+//       1  btn iconImage:(on/off)
+//     6 name: "Видео" type:String
+//     7 name: "Аудио" type:String
+
+// 3 Динамический блок ввода и отображения данных
