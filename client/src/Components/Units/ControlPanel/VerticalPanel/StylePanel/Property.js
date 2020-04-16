bb1e62cd47ebd32525a6abcfd6e001fe64ca0fed
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2020-04-16 15:56:07 +0300

    реализовать dnd через добавление свойства в класс свойств как свойство с высотой

--- client/src/Components/Units/ControlPanel/VerticalPanel/StylePanel/Property.js
+++ client/src/Components/Units/ControlPanel/VerticalPanel/StylePanel/Property.js
@@ -3,6 +3,7 @@ import Icon from "react-icons-kit";
 import { cross } from "react-icons-kit/icomoon/cross";
 import PropertyInputSwitch from "./Switch/PropertyInputSwitch";
 import StringInput from "../Inputs/StringInput";
+
 export default function Property({
   property,
   setProperty: { setName, setValue },
@@ -17,50 +18,27 @@ export default function Property({
   const [prop, setProp] = useState(true);
   // console.log('style', style)
   let div;
-  return prop === true ? (
+  const DropDiv=() => (
     <div
-      draggable
-      // onDragStart={(e) => {
-      //   // e.persist();
-      //   e.dataTransfer.setData("transfer", e.target.id);
-      //   console.log("property", property);
-      // }}
-      onDragOver={(e) => {
-        var targetCoords = div.getBoundingClientRect();
-        var yCoord = e.clientY - targetCoords.top;
-        if (Y < e.pageY ) {
-          console.log("Вниз", yCoord);
-          // setstyle({ ...style, paddingTop: 0, paddingBottom: `${div.offsetHeight}px` });
-          setblock('bottom')
-        } else if (Y > e.pageY) {
-          console.log("Вверх", yCoord);
-          setblock('top')
-          // setstyle({ ...style, paddingTop: `${div.offsetHeight}px`, paddingBottom: 0 });
-        }
-        setY(e.pageY);
-      }}
-      onDrop={(e) => {
-        // e.stopPropagation();
-        setstyle({ ...style, paddingTop: 0, paddingBottom: 0 });
-      }}
-      // onDrag={()=>{
-      //   setProp(false);
-      //   console.log("Seychas");
-      // }}
-      // onDragLeave={(e) => {
-      //   e.stopPropagation();
-      //   console.log("onDragLeave")
-      //   // setstyle({ ...style, paddingTop: 0, paddingBottom: 0 });
-      //   setblock()
-      // }}
-      style={style}
-    >
-      {block==='top'?<div style={{height:"23px"}}  onDragLeave={(e) => {
+      style={{ height: "15px" }}
+      onDragLeave={(e) => {
         e.stopPropagation();
-        console.log("onDragLeave")      
-        setblock()
-      }}/>:null}
+        console.log("onDragLeave");
+        setblock();
+      }}
+      // onDragOver={(e) => e.preventDefault()}
+      onDrop={(e) => {
+        e.stopPropagation();
+        console.log("onDragLeave");
+        setblock();
+      }}
+    />
+  );
+  return prop === true ? (
+    <div>
+      {block === "top" ? <DropDiv/> : null}
       <div
+        draggable
         ref={(n) => (div = n)}
         style={{
           // border: "1px solid #fff",
@@ -68,6 +46,16 @@ export default function Property({
           gridTemplateColumns: "60% 35% 1em",
           borderBottom: "2px solid #55667766",
           background: "rgba(30,40,57,.4)",
+        }}
+        onDragEnter={(e) => {
+          if (Y < e.pageY) {
+            console.log("Вниз");
+            setblock("bottom");
+          } else if (Y > e.pageY) {
+            console.log("Вверх");
+            setblock("top");
+          }
+          setY(e.pageY);
         }}
       >
         <div
@@ -103,11 +91,7 @@ export default function Property({
           <Icon size={"100%"} icon={cross} />
         </div>
       </div>
-      {block==='bottom'?<div style={{height:"23px"}}onDragLeave={(e) => {
-        e.stopPropagation();
-        console.log("onDragLeave")      
-        setblock()
-      }}/>:null}
+      {block === "bottom" ? <DropDiv/> : null}
     </div>
   ) : null;
 }
