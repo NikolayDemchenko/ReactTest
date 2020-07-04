a8ba7c85edd1230f1cc95681be10c7e0a8f92904
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2020-07-04 19:55:38 +0300

    Прикрутить панель атрибутов

--- client/src/Components/ControlPanel/NavigationPanel/CRUDTag/CRUDTag.js
+++ client/src/Components/ControlPanel/NavigationPanel/CRUDTag/CRUDTag.js
@@ -4,14 +4,19 @@ import { cross } from "react-icons-kit/icomoon/cross";
 import Icon from "react-icons-kit";
 import SelectPanel from "../../SelectPanel/SelectPanel";
 import htmlTags from "html-tags";
+import htmlTagsVoid from "html-tags/void";
 import { htmlTags as startTags } from "../../../Class/HtmlCss";
 function CRUDTag({ tag, addTag, removeTag }) {
   // console.log('setPage :>> ', setPage);
+// console.log('htmlTagsVoid :>> ', Array.isArray(htmlTagsVoid) );
+
+console.log('htmlTagsVoid.foreEach(tagVoid=>tagVoid===tag.type) :>> ', htmlTagsVoid.find(tagVoid=>tagVoid===tag.type));
 
   return (
     <div style={{ display: "flex" }}>
       {/* Добавление тега */}
-      <SelectPanel
+    {!htmlTagsVoid.find(tagVoid=>tagVoid===tag.type)&&
+    <SelectPanel
         items={startTags.map((item) => item.value)}
         allItems={htmlTags}
         selectedItem={""}
@@ -21,7 +26,8 @@ function CRUDTag({ tag, addTag, removeTag }) {
             <Icon size={"100%"} icon={plus} />
           </div>
         }
-      />
+      />}
+      
       {/* Удаление тега */}
       <div
         onClick={(e) => {
