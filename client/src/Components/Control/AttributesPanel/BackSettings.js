c7b5a95d02be2ceda2eabb3a2eee5d17ffcaa5f1
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2020-10-26 02:31:15 +0300

    допилить управление страницей

--- client/src/Components/Control/AttributesPanel/BackSettings.js
+++ client/src/Components/Control/AttributesPanel/BackSettings.js
@@ -5,25 +5,26 @@ import preset from "jss-preset-default";
 import PopupInput from "./Inputs/PopupInput/PopupInput";
 const BackSettings = (props) => {
   console.log("props :>> ", props.page.bodyStyle);
-  const setPreview=(color)=>{
+  const setPreview = (color) => {
     jss.setup(preset());
-    const back = jss.createStyleSheet({ body:{"background-color":color}  }).attach();
+    const back = jss
+      .createStyleSheet({ body: { "background-color": color } })
+      .attach();
     document.querySelector("body").classList.add(`${back.classes.body}`);
-  }
-  const setValue=(color)=>{
-    props.setPage(page=>{
-       return {...page, bodyStyle:{'background-color':color}} 
-    })
-  }
+  };
+  const setValue = (color) => {
+    props.setPage((page) => {
+      return { ...page, bodyStyle: { "background-color": color } };
+    });
+  };
   return (
-    <div>
+    <div style={{ display: "flex", margin: "0.5em" }}>
+      <div style={{ paddingRight: "2em" }}>Background</div>
       <PopupInput
         {...{
           value: props.page.bodyStyle["background-color"],
           setValue,
           setPreview,
-        //   setPartPreview,
-        //   setFullPreview,
         }}
         height="1em"
       />
