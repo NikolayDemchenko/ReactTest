f7878cdcdf1fbfb1b1dad9aa4e7f5be33b6fb4f7
NikolayDemchenko <nikolaydemchenko1983@gmail.com>
2021-03-17 20:00:32 +0300

    Доделать App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import ErrorBoundry from './ErrorBoundry';
+import { page as testPage } from './Components/View/Pages/CreateApp';
+import MainMenu from './Components/Control/MainMenu';
+import { Context } from './AppFunction';
+import Editor from './Components/Control/Editor';
+import getPageREST from './Function/ServiceFunction/REST/PageREST';
+const App:React.FC = () => {
+	// Рабочий
+	// const [state, setState] = useState({});
+	// Тестовый
+	const [state, setState] = useState({ page: testPage });
+
+	console.log('state :>> ', state);
+
+	const PageREST = getPageREST(setState);
+
+
+	return (
+		<ErrorBoundry>
+			<Context.Provider value={{ PageREST, state, setState }}>
+				<MainMenu />
+				{state.page && <Editor />}
+			</Context.Provider>
+		</ErrorBoundry>
+	);
+};
+
+export default App;
--- client/src/Types/Application.ts
+++ client/src/Types/Application.ts
@@ -1,4 +1,27 @@
-class Application { 
-    id: string;
-    name: string;
+interface Application {
+  id: string;
+  name: string;
+  domain: string;
+  startPageId: string;
 }
+interface Page {
+  id: string;
+  ApplicationId: string;
+  name: string;
+  //   bodyStyle: object;
+  styles: Style[];
+  nodes: Node[];
+}
+interface Style {
+  id: string;
+  name: string;
+  data: object;
+}
+interface Node {
+  id: string;
+  index: number;
+  parentId: string | null;
+  tag: string;
+  styleId: string;
+}
+export type { Application, Page, Node, Style };
